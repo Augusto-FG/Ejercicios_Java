d594c87f00d9cf756576ddf6ae307bc77b28d11b
Augusto Fernández <79116699+Augusto-FG@users.noreply.github.com>
2021-08-16 00:52:33 -0300

    Update Ej3Variable.java

--- Ej3Variable.java
+++ Ej3Variable.java
@@ -1,3 +1,6 @@
+/* Ejercicio: Realizar un programa que permita intercambiar el valor de dos variables. Por ejemplo, si la variable1 vale 5 y la variable2 vale 12,
+hacer que la variable1 valga 12 y la variable2 valga 5. Tener en cuenta que al asignar un valor a una variable se sobrescribe el valor anterior.*/
+
 
 public class Ej3Variable {
 	 public static void main(String[] args) {
